0a9ec81b36e95988bb2318b3327469f91c9d4bd2
Rpsaman13000 <mitrovic.martin@gmail.com>
2020-05-01 18:41:23 +0200

    Fixing contact bugs

--- client/src/components/layout/ContactInfo.js
+++ client/src/components/layout/ContactInfo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import location from "../../assets/svg/about/pin.svg";
+import clock from "../../assets/svg/about/clock.svg";
+import phone from "../../assets/svg/about/phone.svg";
 
 const ContactInfo = () => {
 	return (
 		<div class="container">
-			<div class="row">
-				<div class="icon d-flex justify-content-center">
+			<div class="row centerRowY">
+				<div class="contactIcon d-flex justify-content-center">
 					<img src={location} />
 				</div>
 				<div class="col align-bottom">
@@ -13,8 +15,20 @@ const ContactInfo = () => {
 				</div>
 			</div>
 			<div class="row justify-content-around">
-				<div class="col-3">00-24</div>
-				<div class="col-6">+381 123456789</div>
+				<div class="col">
+					<div class="contactIcon d-flex justify-content-center">
+						<img src={clock} />
+					</div>
+					00-24
+				</div>
+			</div>
+			<div class="row justify-content-around">
+				<div class="col-6">
+					<div class="contactIcon d-flex justify-content-center">
+						<img src={[phone]} />
+					</div>
+					+381 123456789
+				</div>
 			</div>
 		</div>
 	);
