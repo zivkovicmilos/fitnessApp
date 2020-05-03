ace671f8b845d2aae3dc693e2e6197a1e5d21486
Milos Zivkovic <zm170625d@student.etf.bg.ac.rs>
2020-05-03 14:28:04 +0200

    Fixed the typo

--- models/Profile.js
+++ models/Profile.js
@@ -31,4 +31,4 @@ const ProfileSchema = new mongoose.Schema({
 	],
 });
 
-module.exports = Profile = mongoose.model("profile", UserSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
