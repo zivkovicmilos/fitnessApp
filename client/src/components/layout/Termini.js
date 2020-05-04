8076f6849ab9b56652c459b7f36a193eb750a26d
Rpsaman13000 <mitrovic.martin@gmail.com>
2020-05-04 22:09:22 +0200

    Work on settings page in progres

--- client/src/components/layout/Termini.js
+++ client/src/components/layout/Termini.js
@@ -10,6 +10,11 @@ const Termini = () => {
 					<h1>Moji termini, </h1>
 				</div>
 			</div>
+            <div className="row">
+                {brZak &&
+                    
+                }
+            </div>
 			<div className="row">
 				{!brZak && (
 					<div className="col-12 sad">
