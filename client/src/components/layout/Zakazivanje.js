8c6b67d1f817debe2aafba7d3b45924386e5ab7e
Milos Zivkovic <zm170625d@student.etf.bg.ac.rs>
2020-05-29 03:35:28 +0200

    Got the filter deletion to work on the weekly calendar

--- client/src/components/layout/Zakazivanje.js
+++ client/src/components/layout/Zakazivanje.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Zakazivanje/Table";
 import Navbar from "./Navbar";
 import Jumbo from "./Jumbo";
@@ -9,6 +9,108 @@ const Zakazivanje = () => {
 	const [pilates, setPilates] = useState(true);
 	const [core, setCore] = useState(true);
 	const [cardio, setCardio] = useState(true);
+
+	const insertAfter = (referenceNode, newNode) => {
+		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	};
+
+	const getNext = (time) => {
+		switch (time) {
+			case "17:00":
+				return "17:30";
+			case "17:30":
+				return "18:00";
+			case "18:00":
+				return "18:30";
+			case "18:30":
+				return "19:00";
+			case "19:00":
+				return "19:30";
+			case "19:30":
+				return "20:00";
+			case "20:00":
+				return "20:30";
+			case "20:30":
+				return "21:00";
+			case "21:00":
+				return "21:30";
+			case "21:30":
+				return "22:00";
+		}
+	};
+
+	const wipeElements = (workouts) => {
+		while (workouts.length > 0) {
+			let workout = workouts[0];
+			console.log(workout.rowSpan);
+			try {
+				let parent = workout.parentNode;
+				let refTime = parent.id;
+
+				let newTd = document.createElement("td");
+				insertAfter(workout, newTd);
+
+				//let index = 0;
+				//while ((workout = workout.previousSibling) != null) index++;
+				let index = [].indexOf.call(workout.parentNode.children, workout);
+				console.log(`Index is ${index}`);
+
+				if (workout.rowSpan > 1) {
+					for (let i = 1; i < workout.rowSpan; i++) {
+						let reference = document.getElementById(getNext(refTime));
+						refTime = getNext(refTime);
+
+						// TODO Append at a very specific index
+						//reference.appendChild(document.createElement("td"));
+						reference.insertBefore(
+							document.createElement("td"),
+							reference.childNodes[index]
+						);
+					}
+				}
+
+				parent.removeChild(workout);
+			} catch (err) {
+				console.log(`Error: ${err}`);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (!yoga) {
+			let workouts = document.getElementsByClassName("yoga");
+			console.log(`I got ${workouts.length} workouts!`);
+
+			wipeElements(workouts);
+		}
+	}, [yoga]);
+
+	useEffect(() => {
+		if (!pilates) {
+			let workouts = document.getElementsByClassName("pilates");
+			console.log(`I got ${workouts.length} workouts!`);
+
+			wipeElements(workouts);
+		}
+	}, [pilates]);
+
+	useEffect(() => {
+		if (!core) {
+			let workouts = document.getElementsByClassName("core");
+			console.log(`I got ${workouts.length} workouts!`);
+
+			wipeElements(workouts);
+		}
+	}, [core]);
+
+	useEffect(() => {
+		if (!cardio) {
+			let workouts = document.getElementsByClassName("cardio");
+			console.log(`I got ${workouts.length} workouts!`);
+
+			wipeElements(workouts);
+		}
+	}, [cardio]);
 
 	return (
 		<div className="container-fluid">
@@ -23,7 +125,7 @@ const Zakazivanje = () => {
 			/>
 			<div className="row centerRow">
 				<h1
-					className="pageTitle col-md-12 offset-md-1 offset-1 col 
+					className="pageTitle col offset-1 
 				"
 				>
 					NEDELJNI KALENDAR
@@ -31,68 +133,62 @@ const Zakazivanje = () => {
 			</div>
 			<div className="row centerRowX">
 				<div className="col-xs-3">
-					<div className="filters">
-						<h3>FILTERI</h3>
+					<div className="filtersNew mb-3">
+						<h3 className="mb-0 mr-3">FILTERI</h3>
 						<form>
-							<div className="form-check">
+							<div className="custom-control custom-checkbox">
 								<input
 									type="checkbox"
-									className="form-check-input"
-									id="yoga"
-									defaultChecked={yoga}
+									className="custom-control-input"
+									id="yogaCheck"
+									checked={yoga}
 									onChange={() => {
 										setYoga(!yoga);
 									}}
 								/>
-								<label className="form-check-label" for="yoga">
+								<label className="custom-control-label" htmlFor="yogaCheck">
 									Yoga
 								</label>
 							</div>
-							<div className="form-check">
+							<div className="custom-control custom-checkbox">
 								<input
 									type="checkbox"
-									className="form-check-input"
-									id="pilates"
-									defaultChecked={pilates}
+									className="custom-control-input"
+									id="pilatesCheck"
+									checked={pilates}
 									onChange={() => {
 										setPilates(!pilates);
 									}}
 								/>
-								<label
-									className="form-check-label"
-									for="pilates"
-								>
+								<label className="custom-control-label" htmlFor="pilatesCheck">
 									Pilates
 								</label>
 							</div>
-							<div className="form-check">
+							<div className="custom-control custom-checkbox">
 								<input
 									type="checkbox"
-									className="form-check-input"
-									id="core"
-									defaultChecked={core}
+									className="custom-control-input"
+									id="coreCheck"
+									checked={core}
 									onChange={() => {
 										setCore(!core);
 									}}
 								/>
-								<label className="form-check-label" for="core">
+								<label className="custom-control-label" htmlFor="coreCheck">
 									Core
 								</label>
 							</div>
-							<div className="form-check">
+							<div className="custom-control custom-checkbox">
 								<input
 									type="checkbox"
-									className="form-check-input"
-									id="cardio"
-									defaultChecked={cardio}
+									className="custom-control-input"
+									id="cardioCheck"
+									checked={cardio}
 									onChange={() => {
 										setCardio(!cardio);
 									}}
 								/>
-								<label
-									className="form-check-label"
-									for="cardio"
-								>
+								<label className="custom-control-label" htmlFor="cardioCheck">
 									Cardio
 								</label>
 							</div>
--- client/src/components/layout/Zakazivanje/Table.js
+++ client/src/components/layout/Zakazivanje/Table.js
@@ -1,42 +1,38 @@
 import React, { Fragment, useEffect } from "react";
+import close from "./../../../assets/svg/close.svg";
 
 const Table = () => {
 	return (
 		<Fragment>
+			{/*
+			
 			<button
 				type="button"
 				className="btn btn-primary"
 				data-toggle="modal"
-				data-target="#exampleModal"
+				data-target="#reserveModal"
 			>
 				Launch demo modal
 			</button>
-
-			<div
-				className="modal fade"
-				tabindex="-1"
-				id="exampleModal"
-				role="dialog"
-			>
+			*/}
+			<div className="modal fade" tabIndex="-1" id="reserveModal" role="dialog">
 				<div className="modal-dialog" role="document">
 					<div className="modal-content">
 						<div className="container-fluid">
-							<div className="row modalHead centerRow">
-								REZERVACIJA
+							<div className="row modalHead centerRow justify-content-between">
+								<span>REZERVACIJA</span>
+								<img
+									src={close}
+									className="closeButton"
+									data-toggle="modal"
+									data-target="#reserveModal"
+								/>
 							</div>
 							<div className="row modalBody">
-								<span className="modalBodyTitle">
-									Grupni Pilates trening
-								</span>
-								<span className="modalBodyDate">
-									Utorak, 21.04.2020.
-								</span>
-								<span className="modalBodyTime">
-									14:30 - 15:00
-								</span>
-								<span className="modalBodyNum">
-									Broj učesnika: 15
-								</span>
+								<span className="modalBodyTitle">Grupni Pilates trening</span>
+								<span className="modalBodyDate">Utorak, 21.04.2020.</span>
+								<span className="modalBodyTime">14:30 - 15:00</span>
+								<span className="modalBodyNum">Broj učesnika: 15</span>
 							</div>
 							<div className="row modalBottom">
 								<button
@@ -54,7 +50,7 @@ const Table = () => {
 
 			<div className="table-responsive">
 				<table className="table table-bordered">
-					<thead>
+					<thead className="thead-dark">
 						<tr>
 							<th scope="col">Vreme</th>
 							<th scope="col">Ponedeljak</th>
@@ -66,118 +62,157 @@ const Table = () => {
 							<th scope="col">Nedelja</th>
 						</tr>
 					</thead>
-					<tbody>
-						<tr>
+					<tbody className="thead-dark">
+						<tr id="17:00">
 							<th scope="row">17:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-						</tr>
-						<tr>
-							<th scope="row">17:30</th>
-							<td colSpan="1" rowSpan="2" className="pilates">
-								<span>Grupni Pilates trening</span>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td
+								colSpan="1"
+								rowSpan="2"
+								className="pilates workoutScheduleItem"
+							>
+								Napredni nivo
 							</td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="17:30">
+							<th scope="row">17:30</th>
+							<td
+								colSpan="1"
+								rowSpan="2"
+								className="pilates workoutScheduleItem"
+							>
+								Anatomske i ortopedske vežbe
+							</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr id="18:00">
 							<th scope="row">18:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td colSpan="1" rowSpan="2" className="yoga workoutScheduleItem">
+								Upoznajte yogu
+							</td>
+							<td></td>
+							<td
+								colSpan="1"
+								rowSpan="1"
+								className="cardio workoutScheduleItem"
+							>
+								Kardio za mršavljenje
+							</td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="18:30">
 							<th scope="row">18:30</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="19:00">
 							<th scope="row">19:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td colSpan="1" rowSpan="2" className="core workoutScheduleItem">
+								Core - Šta? Zašto? Kako?
+							</td>
+							<td></td>
+							<td></td>
+							<td
+								colSpan="1"
+								rowSpan="1"
+								className="cardio workoutScheduleItem"
+							>
+								Put do zabavnog kardio treninga
+							</td>
 						</tr>
-						<tr>
+						<tr id="19:30">
 							<th scope="row">19:30</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td colSpan="1" rowSpan="1" className="core workoutScheduleItem">
+								HardCore
+							</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="20:00">
 							<th scope="row">20:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td colSpan="1" rowSpan="1" className="yoga workoutScheduleItem">
+								Yoga za homofoteljuse
+							</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td colSpan="1" rowSpan="1" className="core workoutScheduleItem">
+								Učvrstite core, sačuvajte leđa
+							</td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="20:30">
 							<th scope="row">20:30</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td></td>
+							<td
+								colSpan="1"
+								rowSpan="1"
+								className="pilates workoutScheduleItem"
+							>
+								Vežbe na strunjači
+							</td>
+							<td></td>
+							<td colSpan="1" rowSpan="3" className="yoga workoutScheduleItem">
+								Yoga za powerliftere
+							</td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="21:00">
 							<th scope="row">21:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td
+								colSpan="1"
+								rowSpan="2"
+								className="cardio workoutScheduleItem"
+							>
+								Kružni kardio trening
+							</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="21:30">
 							<th scope="row">21:30</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
-						<tr>
+						<tr id="22:00">
 							<th scope="row">22:00</th>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
-							<td colSpan="1" rowSpan="1"></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
 					</tbody>
 				</table>
